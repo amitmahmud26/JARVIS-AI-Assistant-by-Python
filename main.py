44d4efade6fb854c7ea1a14439f1625e11b31eab
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2023-09-13 04:14:03 +0600

    it can repeat my voice and print

--- main.py
+++ main.py
@@ -6,5 +6,24 @@ def say(text):
     os.system(f'say "{text}"')
 
 
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        try:
+            query = r.recognize_google(audio, language="en")
+            return query
+        except Exception as e:
+            print(e)
+            return "Some error occurred. Sorry JARVIS"
+
+
 if __name__ == '__main__':
-    say("hello i'm jarvis A I")
+    say("Hello, I'm JARVIS.")
+
+    while True:
+        print("Listening...")
+        text = takeCommand().lower()
+        print(text)
+        say(text)
